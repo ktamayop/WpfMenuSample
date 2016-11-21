bd4a0ae0c58f207b5044f7812e477bdcd480bcd7
Karel Tamayo Peña <eddyk832005@gmail.com>
2016-11-21 17:42:49 -0500

    Add project files.

---
+++ WpfMenuSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using Prism.Commands;
+
+namespace WpfMenuSample.ViewModels
+{
+    internal class MainViewModel
+    {
+        public MainViewModel()
+        {
+            RaptManager = new RaptManager
+            {
+                RaptConfiguration = new RaptConfiguration
+                {
+                    RecentFiles = new List<string>
+                    {
+                        "Recent File 1",
+                        "Recent File 2"
+                    }
+                }
+            };
+
+            NewFileCommand = new DelegateCommand(() =>
+            {
+                //Dummy command handler
+            });
+
+            OpenFileCommand = new DelegateCommand(OpenFile);
+        }
+
+        private void OpenFile()
+        {
+        }
+
+        public RaptManager RaptManager { get; set; }
+
+        public ICommand NewFileCommand { get; set; }
+
+        public ICommand OpenFileCommand { get; set; }
+    }
+
+    internal class RaptManager
+    {
+        public RaptManager()
+        {
+            RaptConfiguration = new RaptConfiguration();
+        }
+
+        public RaptConfiguration RaptConfiguration { get; set; }
+    }
+
+    internal class RaptConfiguration
+    {
+        public IEnumerable<string> RecentFiles { get; set; }
+    }
+}
